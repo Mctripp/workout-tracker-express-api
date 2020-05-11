78d9225846e8d39b71a90abf683814c619bda4ef
Mctripp <mctripp94@gmail.com>
2020-05-11 12:12:37 -0400

    Add workout schema + route
    Add workout.js, workout_routes.js
    Initial setup for workout resource

---
+++ app/models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const workoutSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date_time: {
+    type: Date,
+    required: true
+  },
+  isComplete: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Workout', workoutSchema)
